6a16daca04e4832fef0a169a9fdd01559ce02991
tohdj <tohdajun@gmail.com>
2022-12-24 02:16:27 +0800

    Update gpx_cleaner.py

--- gpx_cleaner.py
+++ gpx_cleaner.py
@@ -27,13 +27,16 @@ def run(activity_gpx):
                     # if absolute distance travelled is less than 3m, then the recording could have paused.
                     # time - last must be positive, or td_to_str() will crash.
                     #if (abs(d) < 3) and ((time - last) > datetime.timedelta(seconds=0)):
-                    speed = abs(d) / (time - last).total_seconds()
-                    # use speed instead of absolute distance travelled. if speed is < 0.25m/s then the recording could have paused.
-                    if (speed < 0.25) and ((time - last) > datetime.timedelta(seconds=0)):
-                        print('Pause {}: {}s | {:.3f}m'.format(stops+1, time - last, d))
-                        ret_data['Pause {}'.format(stops+1)] = [time - last, d]
-                        removed += time - last
-                        stops += 1
+                    if ((time - last) > datetime.timedelta(seconds=0)):
+                        speed = abs(d) / (time - last).total_seconds()
+                        # use speed instead of absolute distance travelled. if speed is < 0.25m/s then the recording could have paused.
+                        if (speed < 0.25):
+                            print('Pause {}: {}s | {:.3f}m'.format(stops+1, time - last, d))
+                            ret_data['Pause {}'.format(stops+1)] = [time - last, d]
+                            removed += time - last
+                            stops += 1
+                        else:
+                            tot_dist += d
                     else:
                         tot_dist += d
                 if removed > datetime.timedelta():
